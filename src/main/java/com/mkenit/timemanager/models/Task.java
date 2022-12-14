92c9e96b6386ac350a41d989c5d71634709c52b9
mikarpechenkov <98464767+mikarpechenkov@users.noreply.github.com>
2022-12-14 19:28:55 +0300

    Обновлено

--- src/main/java/com/mkenit/timemanager/models/Task.java
+++ src/main/java/com/mkenit/timemanager/models/Task.java
@@ -4,19 +4,27 @@ import javafx.util.Duration;
 import java.util.GregorianCalendar;
 
 public class Task {
-    String name="Без названия";
-    GregorianCalendar startTime=new GregorianCalendar();
-    Duration duration=Duration.minutes(1);
-    Priority importance=Priority.ORDINARY_IMPORTANT;
-    boolean status=false;
+    String name;
+    GregorianCalendar startTime;
+    Duration duration;
+    Priority importance;
+    boolean status;
+
 
     public Task(String name, GregorianCalendar startTime, Duration duration, Priority importance) {
         this.name = name;
         this.startTime = startTime;
         this.duration = duration;
         this.importance = importance;
+        status=false;
     }
 
-    public Task() {}
+    public Task() {
+        name = "Без названия";
+        startTime = new GregorianCalendar();
+        duration = Duration.minutes(1);
+        importance = Priority.ORDINARY_IMPORTANT;
+        status = false;
+    }
 
 }
