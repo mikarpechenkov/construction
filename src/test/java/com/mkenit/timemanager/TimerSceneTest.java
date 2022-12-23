8d2c0a9bd80ed4bd2debf06921b782662d153293
6ix-button-fighter <86019591+6ix-button-fighter@users.noreply.github.com>
2022-12-24 00:07:26 +0300

    Add files via upload
    tests

---
+++ src/test/java/com/mkenit/timemanager/TimerSceneTest.java
@@ -0,0 +1,14 @@
+package com.mkenit.timemanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimerSceneTest {
+
+    @Test
+    public void initialize() {
+
+    }
+
+}
